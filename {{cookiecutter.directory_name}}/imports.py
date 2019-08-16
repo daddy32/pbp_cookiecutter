e107d0ef6b506213d8a7c24e05751a0d3465d376
Daddy32 <daddy@autistici.org>
2019-08-16 15:13:54 +0200

    Multiple changes:
    - Switching to feather format.
    - importing modin when available
    - Pandas utility: custom describe function
    - Various visualizations trying different libraries

--- {{cookiecutter.directory_name}}/imports.py
+++ {{cookiecutter.directory_name}}/imports.py
@@ -1,7 +1,18 @@
 # Generic libraries
 
 import numpy as np
-import pandas as pd
+
+try:
+    #raise ImportError('Skipping modin import.') # Uncomment when encountering issues, for example this issue: https://github.com/modin-project/modin/issues/468
+    import modin.pandas as pd
+    print('Note: Imported modin.pandas in place of pandas as pd.')
+except ImportError:
+    try:
+        import pandas as pd
+        print('Imported regular pandas')
+    except ImportError:
+        print("Failed to import pandas from any known place")
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -16,6 +27,7 @@ from pathlib import Path
 from utils.geo import *
 from utils.filesystem import *
 from utils.xml import *
+from utils.pandas import *
 
 # Custom default settings
 # from utils.settings import *
---
+++ {{cookiecutter.directory_name}}/utils/pandas.py
@@ -0,0 +1,14 @@
+'''
+Various utilities and wrappers for pandas
+'''
+import pandas as pd
+
+# TODO: 'transpose: bool = True' parameter
+def describe(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Returns transposed describe for the dataframe enriched with column types.
+    '''
+    desc = df.describe(include = 'all').transpose()
+    desc.insert(0, 'type', [df.dtypes[x] for x in desc.index])
+    return desc
+    
